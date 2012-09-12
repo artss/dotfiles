80a910e8ff4e711124437fb3060e95a8d915d95c
Arts <arts@n.local>
2012-09-12 20:25:19 +0400

    bin moved to home/bin

---
+++ home/bin/xfterm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+windowid=`xwininfo -root -tree|grep Xfce4-terminal|grep 19[0-9][0-9]|awk '{print $1}'`
+echo $windowid
+
+if [ "x$windowid" != "x" ]; then
+    isunmapped=`xwininfo -id $windowid|grep IsUnMapped`
+    echo $isunmapped
+    if [ "x$isunmapped" != "x" ]; then
+        xdotool windowmap $windowid
+        xdotool windowsize $windowid 1920 1080
+    else
+        xdotool windowunmap $windowid
+    fi
+else
+    xfce4-terminal --fullscreen
+fi
