48c563a7fdd14eff55b6af87ed8cba4e2c4e63d1
Arts <arts@point.im>
2015-08-13 12:42:36 +0300

    profile.d

---
+++ etc/profile.d/10prompt.sh
@@ -0,0 +1,26 @@
+# Command prompt
+
+__vcs_str() {
+  str=$(__git_ps1 " git:%s")
+  if [ "x$str" == "x" ]; then
+    str=$(hg branch 2> /dev/null | awk '{print $1}')
+    if [ "x$str" == "x" ]; then
+      return
+    else
+      str=$(printf " hg:%s" "$str")
+    fi
+  fi
+  echo "$str"
+}
+
+if [ "`id -u`" -eq 0 ]; then
+  PS1='\[\033[01;31m\]\h\[\033[01;32m\] \W #\[\033[00m\] '
+  PS2='\[\033[01;31m\]>\[\033[00m\] '
+else
+  PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W\[\e[1;90m\]$(__vcs_str)\[\033[01;34m\] \$\[\033[00m\] '
+  #PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W \$\[\033[00m\] '
+  PS2='\[\033[01;32m\]>\[\033[00m\] '
+fi
+
+export PS1
+export PS2
---
+++ etc/profile.d/12aliases.sh
@@ -0,0 +1,39 @@
+# Aliases
+
+# colors
+export LS_OPTIONS='--color=auto'
+#/mceval "`dircolors`"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'
+alias grep='grep --color'
+
+# network
+alias wget='wget -c'
+
+alias less='most'
+
+# some directories
+alias media='cd /media'
+alias mnt='cd ~/mnt'
+alias tmp='cd ~/tmp'
+alias ~='cd ~'
+alias etc='cd /etc'
+
+# other
+alias df='df -h'
+alias mc='mc -d'
+alias tmux='tmux -2'
+
+alias mi='speaker-test -t sine -f 329.6'
+
+# mplayer
+#alias mplayer='mplayer -vo vdpau -vc ffh264vdpau,ffmpeg12vdpau,ffwmv3vdpau,ffvc1vdpau, -zoom -framedrop'
+#alias mplayer='mplayer -zoom -framedrop'
+#alias mplayer='optirun mplayer -zoom -framedrop -osdlevel 3'
+
+if [ "`id -u`" -eq 0 ]; then
+        alias reboot="echo 'If you really want to reboot, use /sbin/reboot directly.'"
+        alias halt="echo 'If you really want to shutdown, use /sbin/halt directly.'"
+        alias shutdown="echo 'If you really want to shutdown, use /sbin/shutdown directly.'"
+fi
