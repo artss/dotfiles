ff1273fa20c3663674a9e3e8f13a47233da13ea5
Artem Sazhin <arts@psto.net>
2016-03-17 08:36:03 +0300

    move terminal to current desktop

--- home/bin/xfterm.sh
+++ home/bin/xfterm.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-windowid=`xwininfo -root -tree|grep Xfce4-terminal|grep 19[0-9][0-9]|awk '{print $1}'`
-echo $windowid
+windowid=`xwininfo -root -tree|grep -i xfce4-terminal|grep 19[0-9][0-9]|awk '{print $1}'`
+#echo $windowid
 
 if [ "x$windowid" != "x" ]; then
     isunmapped=`xwininfo -id $windowid|grep IsUnMapped`
-    echo $isunmapped
+
     if [ "x$isunmapped" != "x" ]; then
+        desktop=`xdotool get_desktop`
+        xdotool set_desktop_for_window $windowid $desktop
         xdotool windowmap $windowid
         xdotool windowsize $windowid 1920 1080
         xdotool windowraise $windowid
