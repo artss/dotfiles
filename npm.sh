f6c584f0303ab8d7bd0089da224532aca6843bd2
Arts <arts@point.im>
2018-02-24 12:19:32 +0300

    npm packages for eslint

---
+++ npm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+npm i -g \
+    eslint \
+    babel-eslint \
+    eslint-config-airbnb \
+    eslint-plugin-import \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-react \
+    stylelint \
+    stylelint-config-css-modules \
+    stylelint-config-standard
