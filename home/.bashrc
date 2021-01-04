d8aae3c5ab1b4ac253e73e07138c12a44f029870
Arts <arts@point.im>
2021-01-04 18:36:10 +0100

    Git aliases

--- home/.bashrc
+++ home/.bashrc
@@ -2,10 +2,10 @@ source /etc/profile
 
 source $HOME/.nvm/nvm.sh
 
-case `tty` in 
-    /dev/tty[1])
-        echo -n "Starting X Window system"
-        echo startx
-    ;;
-esac
+#case `tty` in 
+#    /dev/tty[1])
+#        echo -n "Starting X Window system"
+#        echo startx
+#    ;;
+#esac
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- home/bin/autolock.sh
+++ home/bin/autolock.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-xautolock -time 5 -locker xflock4 -notify 20 -notifier 'xset dpms force off' &
-xautolock -time 7 -locker "systemctl suspend"
+
+exit 0
+
+xset dpms force on
+
+#xautolock \
+#  -time 1 \
+#  -locker "pacmd list-sink-inputs|grep RUNNING || xflock4" \
+#  -notify 20 \
+#  -notifier "notify-send -i /usr/share/icons/Papirus/48x48/apps/gnome-power-manager.svg 'Power' 'Screen is about to be locked'" \
+#  -killtime 10 \
+#  -killer "grep closed /proc/acpi/button/lid/LID0/state && systemctl suspend"
+
+xset s 10 5
+xss-lock \
+  -n "notify-send -i /usr/share/icons/Papirus/48x48/apps/gnome-power-manager.svg 'Power' 'Screen is about to be locked'" \
+   -- \
+   bash -c 'xflock4 &; sleep 5; grep closed /proc/acpi/button/lid/LID0/state && systemctl suspend'
